71dcef97ccca2e7da648606b253a54efee5b2935
riyazthk <er.riyaz2507@gmail.com>
2020-05-26 15:43:43 +0530

    to find out win when tied

--- flipcoin.sh
+++ flipcoin.sh
@@ -5,29 +5,38 @@ coin=$((RANDOM%2))
    while [ $coin -gt -1 ]
    do
     coin=$((RANDOM%2))
-   
+
     if [[ $coin -eq 0 ]]
     then
        $((head++))
     else
        $((tail++))
     fi
-
+    echo "$head"
+    echo "$tail"
     if [[ $head -ge 21 && $tail -ge 21 ]]
     then
+        if [[ $head -gt $tail ]]
+        then
+           val=$(($head - $tail))
+        else
+           val=$(($tail - $head))
+        fi
         if [[ $head -eq $tail ]]
         then
-           echo "both are tied"
-           break
-        elif [[ $head -gt $tail ]]
+           continue
+        elif [[ $head -gt $tail && $val -ge 2 ]]
         then
            echo "head is winner"
            echo "no of times simulate head is $head"
            break
        else
-          echo "tail is winner"
-          echo "no of times simulate tail is $tail"
-          break
-       fi   
+          if [[ $val -ge 2 ]]
+          then
+             echo "tail is winner"
+             echo "no of times simulate tail is $tail"
+             break
+          fi
+       fi
    fi
    done
